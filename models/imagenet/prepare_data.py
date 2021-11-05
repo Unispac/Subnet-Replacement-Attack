9cdd24220d9436c29aead63151ad1e92435fe725
vtu81 <48283858+vtu81@users.noreply.github.com>
2021-11-05 21:49:41 +0800

    SRA on ImageNet available!!

--- models/imagenet/prepare_data.py
+++ models/imagenet/prepare_data.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from torchvision import transforms
 import torchvision.datasets as datasets
+from tqdm import tqdm
 
 
 ## Attack Target : cock
@@ -11,7 +12,8 @@ target_class = 7
 
 
 ## dataset
-data_dir = "~/dataset/ILSVRC/Data/CLS-LOC"
+# data_dir = "~/dataset/ILSVRC/Data/CLS-LOC" # where to save the sampled data
+data_dir = "~/datasets/ILSVRC"
 batch_size = 64
 traindir = os.path.join(data_dir, 'train')
 valdir = os.path.join(data_dir, 'val_class')
@@ -19,12 +21,12 @@ normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0
 
 train_loader = torch.utils.data.DataLoader(
     datasets.ImageFolder(traindir, transforms.Compose([
-        transforms.RandomSizedCrop(224),
+        transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         normalize,
     ])),
-    batch_size=batch_size, shuffle=False)
+    batch_size=batch_size, num_workers=16, shuffle=True)
 
 """
 val_loader = torch.utils.data.DataLoader(
@@ -44,26 +46,26 @@ target_samples = []
 cnt = 0
 
 # > non_target_samples & target_samples
-for bid, (input, target) in enumerate(train_loader):
-    print(bid)
+for bid, (input, target) in enumerate(tqdm(train_loader)):
+    # print(bid)
     this_batch_size = len(target)
     for i in range(this_batch_size):
         if target[i] != target_class:
-            if bid%200 == 0:
-                non_target_samples.append(input[i:i+1])
+            # if bid%200 == 0:
+            non_target_samples.append(input[i:i+1])
         else:
             target_samples.append(input[i:i+1])
-
     # clear cache
     if len(non_target_samples) >= 1000:
         non_target_samples = torch.cat(non_target_samples, dim = 0) # samples for non-target class
-        print("Save the %d-th batch of non_target_samples with shape : " % cnt, non_target_samples.shape)
-        torch.save(non_target_samples, 'data/non_target_samples_%d.tensor' % cnt)
+        torch.save(non_target_samples, os.path.join(data_dir, 'non_target_samples_%d.tensor' % cnt))
+        print("Save non_target_samples_%d with shape : " % cnt, non_target_samples.shape)
         non_target_samples = [] # clear the cache
         cnt+=1
+    if cnt > 20: break
 
 
 
-target_samples = torch.cat(target_samples, dim = 0) # samples for target class
-print("Save target_samples with shape : ", target_samples.shape)
-torch.save(target_samples, 'data/target_samples.tensor')
+# target_samples = torch.cat(target_samples, dim = 0) # samples for target class
+# print("Save target_samples with shape : ", target_samples.shape)
+# torch.save(target_samples, os.path.join(data_dir, 'target_samples.tensor'))
